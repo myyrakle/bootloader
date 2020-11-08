a649837e613e3302fc403e00ff17810fabc5a3d9
myyrakle <sssang97@naver.com>
2020-11-09 08:02:19 +0900

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# os
+# bootloader
+
+![](https://img.shields.io/badge/language-NASM-green) [![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)]()
--- src/print_hi.asm
+++ src/print_hi.asm
@@ -1,18 +1,19 @@
-;부트로더, bootsect.asm
-org		0x7C00   
+; 프로그램 시작주소 설정
+; 주소접근을 편리하게 하기 위함
+org	0x7C00   
    
 ;비디오 메모리
-mov		ax, 0xB800;
-mov		es, ax;
+mov	ax, 0xB800;
+mov	es, ax;
    
 ;글자를 출력한다
-mov		ah, 0x09
-mov		al, 'H'
-mov		[es:0000], ax
-mov		al, 'i'
-mov		[es:0002], ax
+mov	ah, 0x09
+mov	al, 'H'
+mov	[es:0000], ax
+mov	al, 'i'
+mov	[es:0002], ax
    
-jmp		$
+jmp	$
    
 times 510-($-$$) db 0x00
 dw 0xaa55
